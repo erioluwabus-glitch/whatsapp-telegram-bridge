7981caa2e8167da1e1cd5dec9f27480f8c60fb62
erioluwabus-glitch <erioluwabus@gmail.com>
2025-09-27 01:27:01 +0100

    Update logger.js

--- src/logger.js
+++ src/logger.js
@@ -1,12 +1,4 @@
 // src/logger.js
 import pino from 'pino';
-
-const level = process.env.LOG_LEVEL || 'info';
-
-const transport = process.env.NODE_ENV === 'development'
-  ? { target: 'pino-pretty' }
-  : undefined;
-
-const logger = pino({ level, transport });
-
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
 export default logger;
